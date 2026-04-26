d3ad4bb215cd7c9da3724effc6a57485b5bd7b8f
oscar <oscarfndez@gmail.com>
2026-04-26 22:47:24 +0200

    Fix DB name in properties

--- src/main/java/com/oscarfndez/framework/config/SecurityConfiguration.java
+++ src/main/java/com/oscarfndez/framework/config/SecurityConfiguration.java
@@ -1,5 +1,6 @@
 package com.oscarfndez.framework.config;
 
+import com.oscarfndez.framework.core.services.auth.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
